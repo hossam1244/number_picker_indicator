dc53a89bb550bcbd3a6f3138c6bbcdb515e774af
Hossam Rakha <hossamrakha0@gmail.com>
2024-03-16 14:48:30 +0200

    init the required parameters

--- lib/src/number_picker_indicator.dart
+++ lib/src/number_picker_indicator.dart
@@ -3,7 +3,24 @@ library number_picker;
 import 'package:flutter/material.dart';
 
 class NumberPickerIndicator extends StatefulWidget {
-  const NumberPickerIndicator({super.key});
+  // minimum value of the number picker
+  final int? minValue;
+
+  // maximum value of the number picker
+  final int? maxValue;
+
+  // current value of the number picker
+  final int? value;
+
+// callback when the value of the number picker changes
+  final ValueChanged<int>? onChanged;
+
+  const NumberPickerIndicator(
+      {super.key,
+      required this.minValue,
+      required this.maxValue,
+      required this.value,
+      required this.onChanged});
 
   @override
   State<NumberPickerIndicator> createState() => _NumberPickerIndicatorState();
